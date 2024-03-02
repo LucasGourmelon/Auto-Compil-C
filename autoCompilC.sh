a278d4316465b7610bbfecb8c7c0e2372fe6d486
LucasGourmelon <lucasgourmelon12@gmail.com>
2024-03-02 11:46:52 +0100

    Add files

---
+++ autoCompilC.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $(($#!=1)) == 1 ]; then
+    echo 'This script expects one parameter';
+    exit 1;
+fi
+
+length=${#1};
+sub=2;
+length=$(($length-$sub)); 
+
+while inotifywait -qe modify $1; do
+    echo -e '----------';
+    exeName=$(echo $1 | cut -c 1-$length);
+    gcc $1 -o $exeName;
+    ./$exeName;
+    echo -e '----------';
+done
